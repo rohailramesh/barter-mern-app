9d0c3aa4dc6973a2b62d5709a8ab3f01beb1c9dc
Rohail Ramesh <rohailramesh@hotmail.com>
2024-12-20 17:49:51 +0000

    Feat: Auth, Refresh & Access Tokens

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 const PORT = process.env.PORT || 5001;
 const app = express();
 app.use(express.json());
-
+app.use(cookieParser());
 //auth routes
 app.use("/api/auth", authRoutes);
 
