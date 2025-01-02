17048bc6cefbacd224f4efb3ecd4fb1fc8023015
Rohail Ramesh <rohailramesh@hotmail.com>
2025-01-02 16:56:49 +0000

    Fix: FE Env Variables

--- frontend/src/components/OrderSummary.jsx
+++ frontend/src/components/OrderSummary.jsx
@@ -1,4 +1,14 @@
-import React from "react";
+import { motion } from "framer-motion";
+import { useCartStore } from "../stores/useCartStore";
+import { Link } from "react-router-dom";
+import { MoveRight } from "lucide-react";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "../lib/axios";
+
+//load stripe promise from env variable STRIPE_PROMISE_KEY
+// const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PROMISE_KEY);
+console.log(import.meta.env.VITE_STRIPE_KEY);
+console.log(import.meta.env.MODE);
 
 const OrderSummary = () => {
   return <div>Order Summary</div>;
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,19 @@
-import axois from "axios";
-const axiosInstance = axois.create({
-  baseURL: "http://localhost:5001/api",
-  withCredentials: true,
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "/api",
+  withCredentials: true, // send cookies to the server
 });
 
 export default axiosInstance;
+
+// import axois from "axios";
+// const axiosInstance = axois.create({
+//   baseURL: "http://localhost:5001/api",
+//   withCredentials: true,
+// });
+
+// export default axiosInstance;
